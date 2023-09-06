31e94021a0ed92d65284a20637dd1a15ba450059
Seemanta Sarker <91196514+StrangerSeemanta@users.noreply.github.com>
2023-09-06 22:32:30 +0600

    Updated with Project Previews and Used Languages

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -3,11 +3,15 @@ import FacebookAnimation from "./assets/lotties/FacebookIco.json";
 import InstaAnimation from "./assets/lotties/InstaIco.json";
 import GithubAnimation from "./assets/lotties/GithubIco.json";
 import FiverrAnimation from "./assets/lotties/FiverrIco.json";
+import StackOverFlowIco from "./assets/lotties/stackOverflowIco.json"
 import SocialAnime from "./assets/lotties/social.json";
 import SocialTree from "./assets/lotties/socialTree.json";
 import EmailSentSuccess from "./assets/lotties/emailSentSuccess.json";
 import Button from "./components/Common/Button";
 import EmailAnime from "./assets/lotties/emailAnime.json"
+
+import GmailIcon from "./assets/lotties/gmailIcon.json"
+
 import { RefObject, useRef, useState } from "react";
 
 function Contact() {
@@ -16,10 +20,12 @@ function Contact() {
     const igRef = useRef<LottieRefCurrentProps>(null);
     const gitRef = useRef<LottieRefCurrentProps>(null);
     const fiverrRef = useRef<LottieRefCurrentProps>(null);
+    const stackOverflowRef = useRef<LottieRefCurrentProps>(null)
     const socialAnime = useRef<LottieRefCurrentProps>(null);
     const socialTree = useRef<LottieRefCurrentProps>(null);
     const emailSentSuccess = useRef<LottieRefCurrentProps>(null);
     const emailAnime = useRef<LottieRefCurrentProps>(null);
+    const gmailIcon = useRef<LottieRefCurrentProps>(null);
 
     // hooks
     const [showEmailAnime, SetEmailAnimeVisible] = useState(false);
@@ -51,10 +57,20 @@ function Contact() {
                             <h1>Connect With Me</h1>
                             <Lottie style={{ width: "100%", height: "35vh" }} animationData={SocialTree} lottieRef={socialTree} />
                             <div className="social-icons">
-                                <Lottie onMouseEnter={() => { LottiePlay(fbRef) }} lottieRef={fbRef} autoplay={false} loop={false} animationData={FacebookAnimation} id="fbAnimation" className="lottieSocial" />
-                                <Lottie onMouseEnter={() => { igRef.current?.goToAndPlay(43, true) }} lottieRef={igRef} autoplay={true} loop={false} animationData={InstaAnimation} id="igAnimation" className="lottieSocial" />
-                                <Lottie onMouseEnter={() => { LottiePlay(gitRef) }} lottieRef={gitRef} autoplay={false} loop={false} animationData={GithubAnimation} id="gitAnimation" className="lottieSocial" />
-                                <Lottie onMouseEnter={() => { LottiePlay(fiverrRef, 3) }} lottieRef={fiverrRef} autoplay={false} loop={false} animationData={FiverrAnimation} id="fiverrAnimation" className="lottieSocial" />
+                                {/* Facebook Icon */}
+                                <Lottie onClick={() => { window.open("https://www.facebook.com/Stranger.Seemanta") }} onMouseEnter={() => { LottiePlay(fbRef) }} lottieRef={fbRef} autoplay={false} loop={false} animationData={FacebookAnimation} id="fbAnimation" className="lottieSocial" />
+
+                                {/* Instagram Icon */}
+                                <Lottie onClick={() => { window.open("https://www.instagram.com/wisecat_speaking/") }} onMouseEnter={() => { igRef.current?.goToAndPlay(43, true) }} lottieRef={igRef} autoplay={true} loop={false} animationData={InstaAnimation} id="igAnimation" className="lottieSocial" />
+
+                                {/* Github Icon */}
+                                <Lottie onClick={() => { window.open("https://github.com/StrangerSeemanta") }} onMouseEnter={() => { LottiePlay(gitRef) }} lottieRef={gitRef} autoplay={false} loop={false} animationData={GithubAnimation} id="gitAnimation" className="lottieSocial" />
+
+                                {/* Fiverr Icon */}
+                                <Lottie onClick={() => { window.open(" https://www.fiverr.com/shuvosarker933") }} onMouseEnter={() => { LottiePlay(fiverrRef, 3) }} lottieRef={fiverrRef} autoplay={false} loop={false} animationData={FiverrAnimation} id="fiverrAnimation" className="lottieSocial" />
+
+                                {/* Stack Overflows Icon */}
+                                <Lottie onClick={() => { window.open(" https://www.fiverr.com/shuvosarker933") }} onMouseEnter={() => { LottiePlay(stackOverflowRef, 1.4) }} lottieRef={stackOverflowRef} autoplay={true} loop={false} animationData={StackOverFlowIco} id="sfAnimation" className="lottieSocial" />
 
                             </div>
                         </div>
@@ -63,6 +79,9 @@ function Contact() {
                     <div className="layout-tab">
                         <div className="content-holder">
                             <h1>Send Me a mail</h1>
+                            <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}> <Lottie animationData={GmailIcon} style={{ width: "50px", height: "50px" }} loop={false} onComplete={() => { gmailIcon.current?.playSegments([12, 53], true) }} lottieRef={gmailIcon} id="gmailIcon" />
+                                <span style={{ color: "red", fontFamily: "serif", fontSize: "13px" }}>ssworkmail22@gmail.com</span>
+                            </div>
                             <Lottie lottieRef={emailAnime} animationData={EmailAnime} style={{ width: "100%", height: "40vh" }}
                                 onComplete={() => {
                                     lottieComplete ? emailAnime.current?.playSegments([26, 56], true) : emailAnime.current?.playSegments([56, 26], true);
