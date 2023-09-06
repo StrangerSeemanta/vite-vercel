a1f3c1acb32151af7b21de66f77c5dc8a44d0bcd
Seemanta Sarker <91196514+StrangerSeemanta@users.noreply.github.com>
2023-09-06 23:05:50 +0600

    iframe close button lottie added

--- src/components/Common/Iframe.tsx
+++ src/components/Common/Iframe.tsx
@@ -1,12 +1,28 @@
-import { Fragment, useState } from 'react'
-import RoundButton from './RoundButton'
+import { Fragment, useRef, useState } from 'react'
+// import RoundButton from './RoundButton'
+import Lottie, { LottieRefCurrentProps } from "lottie-react"
+import CloseBtn from "./../../assets/lotties/closeAnimation.json"
 export interface IframeProps {
     onCloseBtnCLick?: () => void;
     src: string;
     frameTitle: string;
 }
 function Iframe({ onCloseBtnCLick, src, frameTitle }: IframeProps) {
-    const [showLoader, setLoaderVisible] = useState(true)
+    const [showLoader, setLoaderVisible] = useState(true);
+    const closeBtnRef = useRef<LottieRefCurrentProps>(null)
+
+    const handleMouseEnter = () => {
+        closeBtnRef.current?.stop();
+        closeBtnRef.current?.setSpeed(0.9)
+
+        closeBtnRef.current?.play();
+    }
+
+    const handleLeave = () => {
+        closeBtnRef.current?.stop();
+    }
+    // const handleComplete = () => { closeBtnRef.current?.goToAndStop(0) }
+
     return (
         <Fragment>
             <div className="iframe-container">
@@ -15,7 +31,8 @@ function Iframe({ onCloseBtnCLick, src, frameTitle }: IframeProps) {
                     <iframe onLoad={() => { setLoaderVisible(false) }} src={src}></iframe>
                     {showLoader && <div className="frame-loader"></div>}
                 </div>
-                <RoundButton className='iframe-closeBtn' handleClick={onCloseBtnCLick}>X</RoundButton>
+                {/* <RoundButton className='iframe-closeBtn' handleClick={onCloseBtnCLick}>X</RoundButton> */}
+                <Lottie autoplay={false} onClick={onCloseBtnCLick} onMouseLeave={handleLeave} onMouseEnter={handleMouseEnter} className='iframe-closeBtn' style={{ width: "90px", height: "90px", cursor: "pointer" }} animationData={CloseBtn} loop={false} lottieRef={closeBtnRef} />
             </div>
         </Fragment >
     )
