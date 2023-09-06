cbc49d0653744eda51c3ca642e0d32fe891a5e40
Seemanta Sarker <91196514+StrangerSeemanta@users.noreply.github.com>
2023-09-06 21:30:49 +0600

    Updated with Project Previews and Used Languages

--- src/components/ProjectGallery.tsx
+++ src/components/ProjectGallery.tsx
@@ -4,7 +4,10 @@ import ProjectCard from './Common/ProjectCard';
 import Iframe, { IframeProps } from './Common/Iframe';
 
 
-import thumbnail from "./../assets/side.jpg";
+import AnimatedWebsite_thumbnail from "./../assets/animated_preview.png";
+import Bank_preview from "./../assets/banweb_preview.png";
+import Crazy_preview from "./../assets/crazy_cock_preview.png";
+
 function ProjectGallery() {
     const [showIframe, setIframeVisible] = useState(false);
     const [frameData, setFrameData] = useState({} as IframeProps)
@@ -20,32 +23,32 @@ function ProjectGallery() {
         <Fragment>
             <section id='projectGallery' className="project">
                 <div className='container project-container'>
-                    <ProjectCard thumbnail={thumbnail} cardTitle='Bank Website Dummy' buttonsArr={[
-                        <Button children='Study More' />,
+                    <ProjectCard thumbnail={Bank_preview} cardTitle='Bank Website Dummy' buttonsArr={[
+                        <Button children='Used Languages' />,
                         <Button children='Live Demo ' handleClick={() => {
                             showFrame({ src: "https://strangerseemanta.github.io/bankweb/", frameTitle: "Bank Website Dummy" })
                         }} />
                     ]}>
-                        This is first website
+                        I have recreated the bank design for a client from CANADA. They told me that They need a website with the same design of an existing bank website. And Here it is.
                     </ProjectCard>
 
-                    <ProjectCard thumbnail={thumbnail} cardTitle='Animated Website' buttonsArr={[
-                        <Button children='Study More' />,
+                    <ProjectCard thumbnail={AnimatedWebsite_thumbnail} cardTitle='Animated Website' buttonsArr={[
+                        <Button children='Used Languages' />,
                         <Button children='Live Demo ' handleClick={() => {
                             showFrame({ src: "https://strangerseemanta.github.io/animated/", frameTitle: "Animated Website" })
                         }} />
                     ]}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum odit dolorum magni amet sit nobis veritatis placeat, nesciunt commodi aspernatur officia ad omnis facilis dignissimos ratione rerum ex repudiandae vero delectus itaque consectetur officiis.
+                        This is a parallax website with animation. I made this website with HTML CSS JAVASCRIPT and related libraries. The black kitty will make his journey through a jungle. And this is the website. I will not show your the full website with full functionalities. Here is the sample version.
                     </ProjectCard>
 
 
-                    <ProjectCard thumbnail={thumbnail} cardTitle='Crazy Cock' buttonsArr={[
-                        <Button children='Study More' />,
+                    <ProjectCard thumbnail={Crazy_preview} cardTitle='Crazy Cock' buttonsArr={[
+                        <Button children='Used Languages' />,
                         <Button children='Live Demo ' handleClick={() => {
                             showFrame({ src: "https://strangerseemanta.github.io/crypto_website_demo/", frameTitle: "Crazy Cock" })
                         }} />
                     ]}>
-                        This is the website to show a crypto currency name "Cock Crypto".
+                        This is the website to show a crypto currency name "Cock Crypto". I made the entire website on my own. Ihave used Navigation Bar , Custom Cursor, Slider, Charts. I hope you will enjoy it. This is fully responsive for any device screen.
                     </ProjectCard>
 
                 </div>
